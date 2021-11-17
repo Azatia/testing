daac793b3d44d72e77d244fd105caabd312a24c5
tazastrong <tazastrong@gmail.com>
2021-11-17 18:34:47 +0600

    Лиды

--- src/test/java/Azatia/links.java
+++ src/test/java/Azatia/links.java
@@ -3,6 +3,7 @@ package Azatia;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -106,7 +107,7 @@ public class links {
                 .click();
 
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
 
         // РЕДАКТИРУЕМ ЛИД — Создаем сделку
@@ -180,9 +181,30 @@ public class links {
                 .findElements(By.className("form-group"))
                 .get(0)
                 .findElement(By.className("rb-typeahead-input"))
-                .sendKeys("Azat Ramazanov" + "\n");
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("span"))
+                .click();
 
-        // Непонятно, почему не вышло имя Азат, а другое женское
+        Thread.sleep(1000);
+
+        driver
+                .findElementByClassName("lead-deal-form")
+                .findElements(By.className("form-group"))
+                .get(0)
+                .findElement(By.className("rb-typeahead-input"))
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("input"))
+                .sendKeys("Azat Ramazanov");
+        Thread.sleep(1000);
+
+        driver
+                .findElementByClassName("lead-deal-form")
+                .findElements(By.className("form-group"))
+                .get(0)
+                .findElement(By.className("rb-typeahead-input"))
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("input"))
+                .sendKeys(Keys.ENTER);
 
 
         // Нажимаем на поле Тип договора
@@ -199,11 +221,153 @@ public class links {
 
         Thread.sleep(3000);
 
-        // Заполняем адрес
+        // Заполняем ЖК
         driver
-                .findElement(By.className("sf-address-item"))
-                .findElement(By.className("multiselect__placeholder"))
-                .sendKeys("\n");
+                .findElement(By.className("sf-complex-item"))
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("span"))
+                .click();
+
+        Thread.sleep(1000);
+
+        driver
+                .findElement(By.className("sf-complex-item"))
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("input"))
+                .sendKeys("Vivat");
+
+        Thread.sleep(1000);
+
+        driver
+                .findElement(By.className("sf-complex-item"))
+                .findElement(By.className("multiselect__tags"))
+                .findElement(By.cssSelector("input"))
+                .sendKeys(Keys.ENTER);
+
+        // Заполняем номер квартиры
+
+        driver
+                .findElement(By.className("sf-flat-num-item"))
+                .findElement(By.cssSelector("input"))
+                .sendKeys("808");
+
+        // Переходим в раздел ДОПОЛНИТЕЛЬНАЯ ИНФОРМАЦИЯ
+
+        driver
+                .findElement(By.className("rb-supply-form-secondary-section"))
+                .findElement(By.className("mdi-plus-box"))
+                .click();
+
+        // Нажимаем на санузел
+
+        driver
+                .findElement(By.className("sf-toilet-type-item"))
+                .findElement(By.className("dropdown-toggle"))
+                .click();
+
+        Thread.sleep(1000);
+
+        // Нажимаем на варианты в санузле
+
+        driver
+                .findElement(By.className("sf-toilet-type-item"))
+                .findElement(By.className("dropdown-menu"))
+                .findElements(By.className("dropdown-item"))
+                .get(0)
+                .click();
+
+
+        // Нажимаем на Мебель
+        driver
+                .findElement(By.className("sf-furniture-type-item"))
+                .findElement(By.className("dropdown-toggle"))
+                .click();
+
+
+        Thread.sleep(1000);
+
+        // Выбираем Полностью меблирована
+        driver
+                .findElement(By.className("sf-furniture-type-item"))
+                .findElement(By.className("dropdown-item"))
+                .click();
+
+
+        // Нажимаем на балкон
+        driver
+                .findElement(By.className("sf-balcony-type-item"))
+                .findElement(By.className("dropdown-toggle"))
+                .click();
+
+
+        // Выбираем застеклен
+        driver
+                .findElement(By.className("sf-balcony-type-item"))
+                .findElements(By.className("dropdown-item"))
+                .get(1)
+                .click();
+
+
+        // Нажимаем на класс жилья
+        driver
+                .findElement(By.className("sf-property-class-item"))
+                .findElement(By.className("dropdown-toggle"))
+                .click();
+
+        Thread.sleep(1000);
+
+        // Выбираем класс жилья Комфорт
+        driver
+                .findElement(By.className("sf-property-class-item"))
+                .findElements(By.className("dropdown-item"))
+                .get(3)
+                .click();
+
+
+        // Нажимаем на В Залоге
+        driver
+                .findElement(By.className("sf-bail-item"))
+                .findElement(By.className("rb-false-button"))
+                .click();
+
+        // Нажимаем на Создать
+
+        driver
+                .findElement(By.className("btn-sm"))
+                .click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
